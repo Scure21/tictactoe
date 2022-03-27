b0788708d1558389e71f01b04479f2e386828a99
Stephanie Cure Chegwin <stephanie.cure@willowtreeapps.com>
2022-03-26 20:13:33 -0400

    Add styles folder
    - Move stylesheets from the src folder to a new folder calles styles
    - Change the extension for the css files to sass
    - Update imports for stylesheets in components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import { hot } from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
